c1cd8e255ea90a9bc20ca85cc7d123251fa4b075
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-01-31 20:28:09 +0300

    Update index.php

--- webserver/www/robloxfd/htdocs/index.php
+++ webserver/www/robloxfd/htdocs/index.php
@@ -8,7 +8,7 @@
 <body>
 
     <div style="text-align: center; margin-bottom: 20px;">
-        <h1 style="font-size: 1em;">Go play roblox filtering disabled :P</h1>
+        <h1 style="font-size: 1em;">Go play Roblox Filtering Disabled :P</h1>
     </div>
 
     <div style="max-width: 800px; margin: 0 auto;">
@@ -17,15 +17,15 @@
                 "https://www.youtube.com/embed/ybZR-rITalI",
                 "https://www.youtube.com/embed/WL4Dbxn4CoA",
                 "https://www.youtube.com/embed/s0VbrReYiJA",
-                "https://www.youtube.com/embed/cDj2r8QEzzk"
+                "https://www.youtube.com/embed/cDj2r8QEzzk",
             );
 
             // Select a random video URL from the list
             $randomVideoUrl = $videoList[array_rand($videoList)];
         ?>
 
-        <!-- Embed the random video using an iframe -->
-        <iframe id="randomVideo" width="100%" height="400" src="<?php echo $randomVideoUrl; ?>" frameborder="0" allowfullscreen></iframe>
+        <!-- Embed the random video using an iframe with a larger height -->
+        <iframe id="randomVideo" width="100%" height="1000" src="<?php echo $randomVideoUrl; ?>" frameborder="0" allowfullscreen></iframe>
 
         <script>
             // JavaScript to change the video URL on page refresh
