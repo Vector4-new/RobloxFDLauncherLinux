8ac5110c49514b5791ebda3a5cda536c63a67116
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-03-23 01:43:53 +0300

    Update Unrelated_Installer.sh

--- ManuelJoinScripts/Unrelated_Installer.sh
+++ ManuelJoinScripts/Unrelated_Installer.sh
@@ -8,6 +8,7 @@ chmod +x /home/$USER/RobloxFDLauncherLinux/rfdextraction/xampp-linux-x64-8.2.12-
 aria2c https://archive.org/download/filtering-disabled_202403/filtering-disabled_202403_archive.torrent --dir=/home/$USER/RobloxFDLauncherLinux/rfdextraction --seed-time=0
 unzip /home/$USER/RobloxFDLauncherLinux/rfdextraction/filtering-disabled_202403/FilteringDisabled.zip -d /home/$USER/RobloxFDLauncherLinux/rfdextraction
 sudo cp -r /home/$USER/RobloxFDLauncherLinux/rfdextraction/FilteringDisabled/bin/Webserver /home/$USER/RobloxFDLauncherLinux/
+sudo chown -R $USER /home/$USER/RobloxFDLauncherLinux/Webserver
 sudo mv /home/$USER/RobloxFDLauncherLinux/Webserver /home/$USER/RobloxFDLauncherLinux/Webserver_2022M
 sudo cp -r /home/$USER/RobloxFDLauncherLinux/rfdextraction/FilteringDisabled/bin/Clients /home/$USER/RobloxFDLauncherLinux/
 sudo cp -r /home/$USER/RobloxFDLauncherLinux/rfdextraction/FilteringDisabled/bin/shared /home/$USER/RobloxFDLauncherLinux/
@@ -18,6 +19,10 @@ sudo rm -r /opt/lampp/htdocs
 sudo mkdir /opt/lampp/htdocs
 sudo rm -r /home/$USER/RobloxFDLauncherLinux/rfdextraction
 sudo cp /home/$USER/RobloxFDLauncherLinux/index.php /home/$USER/RobloxFDLauncherLinux/Webserver_2022M/www/
-sudo cp -a /home/twig/RobloxFDLauncherLinux/Webserver_2022M/www/. /opt/lampp/htdocs
+sudo cp -a /home/$USER/RobloxFDLauncherLinux/Webserver_2022M/www/. /opt/lampp/htdocs
+sudo chown -R $USER /home/$USER/RobloxFDLauncherLinux/shared
+sudo chown -R $USER /home/$USER/RobloxFDLauncherLinux/Clients
+sudo chown -R $USER /opt/lampp/htdocs/
+sudo rm /home/$USER/RobloxFDLauncherLinux/index.php
 echo RFD is now installed for further info view https://github.com/Vector4-new/RobloxFDLauncherLinux/ or README.MD
 sleep 5
