d440f0e0c78bf95394b8151395a573bc64280a13
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-03-23 22:01:49 +0300

    Update 2022M_HostScript.sh

--- ManuelHostScripts/2022M_HostScript.sh
+++ ManuelHostScripts/2022M_HostScript.sh
@@ -1,1 +1,1 @@
-wine /home/$USER/RobloxFDLauncherLinux/Clients/2022M/2022_Host.bat
+wine /home/$USER/RobloxFDLauncherLinux/Clients/2022M/RobloxStudioBeta.exe -localPlaceFile maps\2007Crossroads.rbxl -task StartServer -port 2005
