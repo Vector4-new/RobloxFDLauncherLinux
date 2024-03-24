fc4e68fdcb70953e47991edd5b3178b3d5312077
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-03-24 03:36:33 +0300

    Update 2022M_JoinScript.sh

--- ManuelJoinScripts/2022M_JoinScript.sh
+++ ManuelJoinScripts/2022M_JoinScript.sh
@@ -1,1 +1,2 @@
+echo "Please run Webserver_2022M_start.sh if you haven't"
 wine /home/$USER/RobloxFDLauncherLinux/Clients/2022M/RobloxStudioBeta.exe -task StartClient -server "127.0.0.1" -port 2005"
