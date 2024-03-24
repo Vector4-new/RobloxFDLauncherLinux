60d1d35b7a8a7e5c942ca6ba9d3a267c544f0599
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-03-24 21:05:55 +0300

    Create 2022M_JoinScript.sh

---
+++ ManuelJoinScripts/2022M_JoinScript.sh
@@ -0,0 +1,2 @@
+echo "Please run Webserver_2022M_start.sh before running this"
+wine "/home/$USER/RobloxFDLauncherLinux/Clients/2022M/RobloxStudioBeta.exe" -task StartClient -server "127.0.0.1" -port 2005
