79eb50687d04bdb1f13c51535a9f469ab63de086
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-03-24 21:03:25 +0300

    Create 2022M_HostScript.sh

---
+++ ManuelHostScripts/2022M_HostScript.sh
@@ -0,0 +1,2 @@
+echo "Please run Webserver_2022M_start.sh if you haven't"
+wine "/home/$USER/RobloxFDLauncherLinux/Clients/2022M/RobloxStudioBeta.exe" -localPlaceFile 2022M_Maps/2007Crossroads.rbxl -task StartServer -port 2005
