91a30da96651ad1b964327dc3afe1f7a2a9b7db9
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-04-14 21:05:30 +0300

    Update 2022M_HostScript.sh

--- ManuelHostScripts/2022M_HostScript.sh
+++ ManuelHostScripts/2022M_HostScript.sh
@@ -1,2 +1,11 @@
-echo "Please run Webserver_2022M_start.sh if you haven't"
-wine "/home/$USER/RobloxFDLauncherLinux/Clients/2022M/RobloxStudioBeta.exe" -localPlaceFile 2022M_Maps/2007Crossroads.rbxl -task StartServer -port 2005
+#!/bin/bash
+
+# Ask user for IP address, port number, and username
+map=$(whiptail --inputbox "Enter mapname without file extension:" 8 60 --title "Map file name" 3>&1 1>&2 2>&3)
+port=$(whiptail --inputbox "Enter port number:" 8 60 --title "Port Number" 3>&1 1>&2 2>&3)
+winebin=$(cat /home/$USER/RobloxFDLauncherLinux/settings/winebin.txt)
+
+echo "Don't forget to start the 2022M webserver!" 
+sleep 3
+cd /home/$USER/RobloxFDLauncherLinux/ManuelHostScripts
+"$winebin" /home/$USER/RobloxFDLauncherLinux/Clients/2022M/RobloxStudioBeta.exe -localPlaceFile "2022M_Maps/$map.rbxl" -task StartServer -port "$port"
