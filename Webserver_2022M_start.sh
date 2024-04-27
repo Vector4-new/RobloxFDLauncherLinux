86364474f39e55c45c2ac1e2de11a144cf8a6b9e
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-04-27 15:00:24 +0300

    Update Webserver_2022M_start.sh

--- Webserver_2022M_start.sh
+++ Webserver_2022M_start.sh
@@ -1,3 +1,4 @@
 sudo /opt/lampp/lampp stop
 /home/$USER/RobloxFDLauncherLinux/webserver/stop.sh
-sudo wine /home/$USER/RobloxFDLauncherLinux/Webserver_2022M/UwAmp.exe
+winebin=$(cat /home/$USER/RobloxFDLauncherLinux/settings/winebin_2022M.txt)
+sudo $winebin /home/$USER/RobloxFDLauncherLinux/Webserver_2022M/UwAmp.exe
