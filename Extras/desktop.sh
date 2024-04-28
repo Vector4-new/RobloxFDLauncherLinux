5c2ac0a61476da9ea625d134d080267b68061a63
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-04-28 13:19:13 +0300

    Update desktop.sh

--- Extras/desktop.sh
+++ Extras/desktop.sh
@@ -2,14 +2,17 @@
 read -p "Enter your username: " username
 
 goverlay_desktop_file="/home/$USER/.local/share/applications/rfd.desktop"
-sudo echo "[Desktop Entry]" > "$goverlay_desktop_file"
-sudo echo "Name=Play Roblox Filtering Disabled" >> "$goverlay_desktop_file"
-sudo echo "Exec=exec /home/$USER/RobloxFDLauncherLinux/joinnew.sh"
-sudo echo "Icon=/home/$USER/.local/share/applications/RFD.png" >> "$goverlay_desktop_file"
-sudo echo "Type=Application" >> "$goverlay_desktop_file"
-sudo echo "Categories=Games;" >> "$goverlay_desktop_file"
+sudo tee "$goverlay_desktop_file" > /dev/null <<EOF
+[Desktop Entry]
+Name=Play Roblox Filtering Disabled
+Terminal=true
+Exec=/home/$USER/RobloxFDLauncherLinux/joinnew.sh
+Icon=/home/$USER/.local/share/applications/RFD.png
+Type=Application
+Categories=Games;
+EOF
 
 # Replace USER_PLACEHOLDER with the actual username
 sed -i "s|USER_PLACEHOLDER|$username|g" "$goverlay_desktop_file"
-echo "\e[0;31mRFD is now installed for further info view https://github.com/Vector4-new/RobloxFDLauncherLinux/ or README.MD.\e"
+echo -e "\e[0;31mRFD is now installed. For further info, view https://github.com/Vector4-new/RobloxFDLauncherLinux/ or README.MD.\e"
 sleep 5
