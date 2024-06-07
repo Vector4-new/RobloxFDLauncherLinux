d5e97cec12bd342d70df85bf254b3d613eeac0f8
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-06-07 18:34:05 +0300

    Update RFDWindowsVersion_Installer.sh

--- Extras/RFDWindowsVersion_Installer.sh
+++ Extras/RFDWindowsVersion_Installer.sh
@@ -8,7 +8,7 @@ sudo pacman -S p7zip --noconfirm
 echo "RFD Windows version install script // Make sure you have Wine, wget and aria2c installed."
 sleep 5
 aria2c https://archive.org/download/roblox-unfiltered/roblox-unfiltered_archive.torrent --dir=/home/$USER --seed-time=0
-7z /home/$USER/roblox-unfiltered/FilteringDisabled.7z -o/home/$USER
+7z x /home/$USER/roblox-unfiltered/FilteringDisabled.7z -o/home/$USER
 
 #RM Stuff
 sudo rm -r /home/$USER/roblox-unfiltered
