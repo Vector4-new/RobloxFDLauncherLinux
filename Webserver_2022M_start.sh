8f127ef819b259b927225291547f0d4b7870ae21
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-07-26 20:37:46 +0300

    Delete Webserver_2022M_start.sh

--- Webserver_2022M_start.sh
+++
@@ -1,4 +0,0 @@
-sudo /opt/lampp/lampp stop
-/home/$USER/RobloxFDLauncherLinux/webserver/stop.sh
-winebin=$(cat /home/$USER/RobloxFDLauncherLinux/settings/winebin_2022M.txt)
-sudo $winebin /home/$USER/RobloxFDLauncherLinux/Webserver_2022M/UwAmp.exe
